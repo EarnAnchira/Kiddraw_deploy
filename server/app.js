9e511d4280cf88492669b036533792a3cc587ca9
intern_earn <earnearn.anchira@gmail.com>
2023-05-02 22:57:38 +0700

    no message

--- server/app.js
+++ server/app.js
@@ -15,6 +15,22 @@ app.use(cors());
 app.use("/uploads",express.static("./uploads"))
 app.use("/api/v1",router);
 
+app.get("/getdata", (req, res) => {
+    const query = req.query.q;
+    try {
+        conn.query(`SELECT * FROM Story WHERE StoryTitleEng LIKE '%${query}'`, (err, result) => {
+            if (err) {
+                console.log("error")
+            } else {
+                console.log("data get")
+                res.status(201).json({ status: 201, data: result })
+            }
+        })
+    } catch (error) {
+        res.status(422).json({ status: 422, error })
+    }
+});
+
 app.listen(process.env.PORT||port,()=>{
     console.log("server start")
 })
